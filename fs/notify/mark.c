bc21828d1e297e986bf0446a5f1f58b3ea83b2c8
Tim Gardner <tim.gardner@canonical.com>
2016-01-12 10:22:38 -0700

    Revert "UBUNTU: SAUCE: AUFS"
    This reverts commit dc2781ca67ef072fff4494c4ca50acf901887cfb.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/notify/mark.c
+++ fs/notify/mark.c
@@ -113,7 +113,6 @@ void fsnotify_put_mark(struct fsnotify_mark *mark)
 		mark->free_mark(mark);
 	}
 }
-EXPORT_SYMBOL(fsnotify_put_mark);
 
 /* Calculate mask of events for a list of marks */
 u32 fsnotify_recalc_mask(struct hlist_head *head)
@@ -214,7 +213,6 @@ void fsnotify_destroy_mark(struct fsnotify_mark *mark,
 	mutex_unlock(&group->mark_mutex);
 	fsnotify_free_mark(mark);
 }
-EXPORT_SYMBOL(fsnotify_destroy_mark);
 
 void fsnotify_destroy_marks(struct hlist_head *head, spinlock_t *lock)
 {
@@ -400,7 +398,6 @@ err:
 
 	return ret;
 }
-EXPORT_SYMBOL(fsnotify_add_mark);
 
 int fsnotify_add_mark(struct fsnotify_mark *mark, struct fsnotify_group *group,
 		      struct inode *inode, struct vfsmount *mnt, int allow_dups)
@@ -501,7 +498,6 @@ void fsnotify_init_mark(struct fsnotify_mark *mark,
 	atomic_set(&mark->refcnt, 1);
 	mark->free_mark = free_mark;
 }
-EXPORT_SYMBOL(fsnotify_init_mark);
 
 static void fsnotify_mark_destroy(struct work_struct *work)
 {
