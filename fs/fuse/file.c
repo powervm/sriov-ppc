810040a72832d87fee16683f7d853587e10cab76
Tim Gardner <tim.gardner@canonical.com>
2016-04-20 14:35:53 -0600

    Revert "UBUNTU: SAUCE: (noup) fuse: do not use iocb after it may have been freed"
    BugLink: http://bugs.launchpad.net/bugs/1572722
    
    This reverts commit 41506ac0665dcb99ea113147eaac219f32499e46.
    
    use upstream stable patch
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2854,7 +2854,6 @@ fuse_direct_IO(struct kiocb *iocb, struct iov_iter *iter, loff_t offset)
 	loff_t i_size;
 	size_t count = iov_iter_count(iter);
 	struct fuse_io_priv *io;
-	bool is_sync = is_sync_kiocb(iocb);
 
 	pos = offset;
 	inode = file->f_mapping->host;
@@ -2894,11 +2893,11 @@ fuse_direct_IO(struct kiocb *iocb, struct iov_iter *iter, loff_t offset)
 	 * to wait on real async I/O requests, so we must submit this request
 	 * synchronously.
 	 */
-	if (!is_sync && (offset + count > i_size) &&
+	if (!is_sync_kiocb(iocb) && (offset + count > i_size) &&
 	    iov_iter_rw(iter) == WRITE)
 		io->async = false;
 
-	if (io->async && is_sync)
+	if (io->async && is_sync_kiocb(iocb))
 		io->done = &wait;
 
 	if (iov_iter_rw(iter) == WRITE) {
@@ -2912,7 +2911,7 @@ fuse_direct_IO(struct kiocb *iocb, struct iov_iter *iter, loff_t offset)
 		fuse_aio_complete(io, ret < 0 ? ret : 0, -1);
 
 		/* we have a non-extending, async request, so return */
-		if (!is_sync)
+		if (!is_sync_kiocb(iocb))
 			return -EIOCBQUEUED;
 
 		wait_for_completion(&wait);
