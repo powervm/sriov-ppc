e4c2ea2fe17dc5af5e46de79ff067d61cb7d240a
Juan J. Alvarez <jjalvare@us.ibm.com>
2017-10-18 11:22:59 -0500

    pci/iov: Expose VF drivers Auto Probe Globally
    When SR-IOV is supported in pci_dev struct the
    member variable drivers_autoprobe controls the
    VF drivers auto probe functionality. This
    patch exposes a gloabal function to be used
    if needed when configuring the pci_dev sriov object.

--- drivers/pci/iov.c
+++ drivers/pci/iov.c
@@ -637,6 +637,18 @@ void pci_restore_iov_state(struct pci_dev *dev)
 }
 
 /**
+ * pci_vf_drivers_autoprobe - set pf property drivers_autoprobe for vfs
+ * @dev: the PCI device
+ * @auto_probe: set vf drivers auto probe flag
+ */
+void pci_vf_drivers_autoprobe(struct pci_dev *dev, bool auto_probe)
+{
+	if (dev->is_physfn)
+		dev->sriov->drivers_autoprobe = auto_probe;
+}
+EXPORT_SYMBOL_GPL(pci_vf_drivers_autoprobe);
+
+/**
  * pci_iov_bus_range - find bus range used by Virtual Function
  * @bus: the PCI bus
  *
